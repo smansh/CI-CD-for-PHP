84c9712e284740a55aa247d0aae061848f3e8c91
manoj <manoj.himt@gmail.com>
2022-08-16 18:35:59 +0530

    trigger9

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo "<h1> <font color=red>Build Trigger using hook when changed in github and deploy to server2</font> </h1>";
+echo "<h1> <font color=red>Build Trigger using hook when changed in github and deploy to server</font> </h1>";
 
 ?>
